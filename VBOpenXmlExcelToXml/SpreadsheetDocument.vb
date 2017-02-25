483d0e1c1d241eb38b6cf3f6ce6fa1e99dd1784c
myersm <myersm@MYERSM1>
2017-02-24 23:25:49 -0700

    Add project files.

---
+++ VBOpenXmlExcelToXml/SpreadsheetDocument.vb
@@ -0,0 +1,15 @@
+﻿Imports VBOpenXmlExcelToXml
+
+Friend Class SpreadsheetDocument
+    Friend Id As Object
+
+    Friend Shared ReadOnly Property Open(filename As String, v As Boolean) As SpreadsheetDocument
+        Get
+            Throw New NotImplementedException()
+        End Get
+    End Property
+
+    Friend Function WorkbookPart() As Object
+        Throw New NotImplementedException()
+    End Function
+End Class
